0032ddd9b1e5c65f504b8927c91692bbb79e84c7
nesrine-benlarbi <nesrine.benlarbi@laplateforme.io>
2025-10-20 15:48:11 +0200

    Mon Runtrack 3

---
+++ jour01/job01/index.php
@@ -0,0 +1,21 @@
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mon premier test JavaScript</title>
+
+    <!--  Ici je mets mon script JavaScript directement dans la balise <head> -->
+    <script>
+        //  Ce code s’exécute quand la page se charge
+        // Il affiche une petite fenêtre (popup) avec le message “Hello Javascript!”
+        alert("Hello Javascript!");
+    </script>
+</head>
+
+<body>
+   
+</body>
+</html>
